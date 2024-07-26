810bb98be6134a5fb3114cf62410195113807555
iahmedgamal <ahmad.g.mustafa@gmail.com>
2024-07-26 16:17:25 +0300

    fixing a bug

--- models/cart.js
+++ models/cart.js
@@ -1,42 +1,42 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 const rootDir = require("../util/path");
 
 const p = path.join(
     rootDir,
     'data',
     'cart.json'
-    )
-module.exports = class Cart{
-    static addProduct(id, productPrice){
-        // fetch the previous cart
-        fs.readFile(p, (err, fileContent)=>{
-            let cart = {products:[], totalPrice: 0}
-            if(!err){
-                cart = JSON.parse(fileContent)
+);
+
+module.exports = class Cart {
+    static addProduct(id, productPrice) {
+        fs.readFile(p, (err, fileContent) => {
+            let cart = { products: [], totalPrice: 0 };
+            if (!err) {
+                cart = JSON.parse(fileContent);
             }
-            console.log(cart)
-            const exisitingProductIndex = cart.products.findIndex(prod=> prod.id == id)
-            const exisitingProduct= cart.products[exisitingProductIndex]
+            console.log(cart);
+
+            const existingProductIndex = cart.products.findIndex(prod => prod.id == id);
+            const existingProduct = cart.products[existingProductIndex];
             let updatedProduct;
-            if(exisitingProduct){
-                updatedProduct = {...exisitingProduct}
-                updatedProduct.qty = updatedProduct.qty + 1
-                cart.products = [...cart.products]
-                cart.products[exisitingProductIndex] = updatedProduct
-            }else{
-                updatedProduct = {id: id, qty: 1}
+
+            if (existingProduct) {
+                updatedProduct = { ...existingProduct, qty: existingProduct.qty + 1 };
+                cart.products[existingProductIndex] = updatedProduct;
+            } else {
+                updatedProduct = { id: id, qty: 1 };
+                cart.products.push(updatedProduct);
             }
-            cart.totalPrice = cart.totalPrice + productPrice
-            cart.products = [...cart.products, updatedProduct]
-            fs.writeFile(p, JSON.stringify(cart), (err)=>{
-                console.log("err", err)
-                if(!err){
-                    console.log("done")
+
+            cart.totalPrice = cart.totalPrice + productPrice;
+
+            fs.writeFile(p, JSON.stringify(cart), (err) => {
+                console.log("err", err);
+                if (!err) {
+                    console.log("done");
                 }
-            })
-        })
-        // analize the cart => find the exisiting product 
+            });
+        });
     }
- 
-}
+};
